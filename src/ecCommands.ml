b9737f3f48b86e0e661a7688287bc91fa3cffc8e
Pierre-Yves Strub <pierre-yves@strub.nu>
2015-07-21 23:06:09 +0200

    Add a pragma to restart EasyCrypt.
    The pragma is `restart`. W.r.t `reset`, `restart` forces the reload of
    the prelude.

--- src/ecCommands.ml
+++ src/ecCommands.ml
@@ -12,17 +12,22 @@ module Sid = EcIdent.Sid
 module Mx  = EcPath.Mx
 
 (* -------------------------------------------------------------------- *)
+exception Restart
+
+(* -------------------------------------------------------------------- *)
 type pragma = {
   pm_verbose : bool; (* true  => display goal after each command *)
   pm_g_prall : bool; (* true  => display all open goals *)
   pm_check   : [`Check | `WeakCheck | `Report];
 }
 
-let pragma = ref {
+let dpragma = {
   pm_verbose = true  ;
   pm_g_prall = false ;
   pm_check   = `Check;
 }
+
+let pragma = ref dpragma
 
 let pragma_verbose (b : bool) =
   pragma := { !pragma with pm_verbose = b; }
@@ -224,7 +229,7 @@ let process_print scope p =
   process_pr Format.std_formatter scope p
 
 (* -------------------------------------------------------------------- *)
-exception Pragma of [`Reset]
+exception Pragma of [`Reset | `Restart]
 
 (* -------------------------------------------------------------------- *)
 let rec process_type (scope : EcScope.scope) (tyd : ptydecl located) =
@@ -436,6 +441,7 @@ and process_pragma (scope : EcScope.scope) opt =
   | "noop"    -> ()
   | "compact" -> Gc.compact ()
   | "reset"   -> raise (Pragma `Reset)
+  | "restart" -> raise (Pragma `Restart)
   | _         -> ()
 
 (* -------------------------------------------------------------------- *)
@@ -582,8 +588,9 @@ let push_context scope context =
       |> omap (fun st -> context.ct_current :: st); }
 
 (* -------------------------------------------------------------------- *)
-let initialize ~undo ~boot ~checkmode =
-  assert (!context = None);
+let initialize ~restart ~undo ~boot ~checkmode =
+  assert (restart || EcUtils.is_none !context);
+  if restart then pragma := dpragma;
   context := Some (rootctxt ~undo (initial ~checkmode ~boot))
 
 (* -------------------------------------------------------------------- *)
@@ -631,8 +638,9 @@ let process ?(timed = false) (g : global_action located) : unit =
     oscope |> oiter (fun scope -> context := Some (push_context scope current));
     if tdelta >= 0. then
       EcScope.notify scope `Info "time: %f" tdelta
-  with Pragma `Reset ->
-    reset ()
+  with
+  | Pragma `Reset   -> reset ()
+  | Pragma `Restart -> raise Restart
 
 (* -------------------------------------------------------------------- *)
 module S = EcScope
