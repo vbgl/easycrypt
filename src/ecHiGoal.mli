3fd09bb8035389287238bdd1af3cd07b3d2b7525
Pierre-Yves Strub <pierre-yves@strub.nu>
2015-12-08 10:27:05 +0100

    Clear generalization pattern.
    Syntax is `move: {id1 ... idn}` and can be followed by regular
    generalization patterns.

--- src/ecHiGoal.mli
+++ src/ecHiGoal.mli
@@ -74,7 +74,7 @@ val process_assumption  : backward
 val process_intros      : ?cf:bool -> ttenv -> intropattern -> backward
 val process_mintros     : ?withbd:bool -> ?cf:bool -> ttenv -> intropattern -> tactical
 val process_generalize  : genpattern list -> backward
-val process_move        : ppterm list -> genpattern list -> backward
+val process_move        : ppterm list -> psymbol list -> genpattern list -> backward
 val process_clear       : psymbol list -> backward
 val process_smt         : ?loc:EcLocation.t -> ttenv -> pprover_infos -> backward
 val process_apply       : implicits:bool -> apply_t -> backward
